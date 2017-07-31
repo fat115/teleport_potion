97cd30fd3f069c33c718c7d03b2ec7cdc9cda2c0
fat115 <fat115@framasoft.org>
2017-07-31 16:40:43 +0200

    updated intllib method (intllib.Getter deprecated)

--- init.lua
+++ init.lua
@@ -5,19 +5,9 @@
 -- and step onto pad or walk into the blue portal light, portal closes after
 -- 10 seconds, pad remains, potions are throwable...  SFX are license Free...
 
--- Intllib
-local S
-
-if minetest.get_modpath("intllib") then
-	S = intllib.Getter()
-else
-	S = function(s, a, ...) a = {a, ...}
-		return s:gsub("@(%d+)", function(n)
-			return a[tonumber(n)]
-		end)
-	end
-
-end
+-- Load support for intllib.
+local MP = minetest.get_modpath(minetest.get_current_modname())
+local S, NS = dofile(MP.."/intllib.lua")
 
 
 -- max teleport distance
